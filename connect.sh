bf9db1dac4df02fd72eede9f5bfd156d575b108b
Ammon Knaupp <adknaupp@gmail.com>
2023-12-22 22:36:52 -0700

    Able to connect over sftp using password.

--- connect.sh
+++ connect.sh
@@ -1,3 +1,10 @@
-ssh-keygen -R [localhost]:8000
+known_host_record="[localhost]:8000 ssh-rsa $(cat keys/host_rsa_key.pub)"
 
-echo "yes" | ssh -i key sftpuser@localhost -p 8000 -
+if ! grep -q "$known_host_record" ~/.ssh/known_hosts; then
+    echo "$known_host_record" >> ~/.ssh/known_hosts
+fi
+
+# singularity run --fakeroot sftp.sif sftp_server.def
+
+ssh -i keys/user_rsa_key -p 8000 sftpuser@localhost
+
