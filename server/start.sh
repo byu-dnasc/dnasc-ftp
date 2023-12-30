8193e9097914d31d90d09a9cd06d7bca4384f925
Ammon Knaupp <adknaupp@gmail.com>
2023-12-30 13:10:42 -0700

    Make $HOME writable

--- server/start.sh
+++ server/start.sh
@@ -1,11 +1,12 @@
 singularity instance start \
+  --containall \
   sftp.sif \
   $CONTAINER
 
 container_output_file="$HOME/.singularity/instances/logs/$HOSTNAME/$USER/${CONTAINER}.out"
 if [ -s "$container_output_file" ]; then
   echo "$0: From container log:"
-  awk '{print "# "$0}' "$container_output_file"
+  awk '{print "+ "$0}' "$container_output_file"
   last_line=$(tail -n 1 $container_output_file)
   if echo "$last_line" | grep -q "exit" ; then
     echo "$0 will now stop the container."
